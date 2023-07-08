4c213a1a0f46a4dbcbbac5489e81515208dbcd15
ZhengJianwei2 <zjw@zjut.edu.cn>
2023-07-08 14:37:25 +0800

    Add files via upload

---
+++ models/CICNet.py
@@ -0,0 +1,371 @@
+import torch
+import torch.nn as nn
+from .resnet import resnet18,resnet34
+import torch.nn.functional as F
+import numpy as np
+import math
+from torch import nn, einsum
+from einops import rearrange
+
+def init_weights(m):
+    """
+    Initialize weights of layers using Kaiming Normal (He et al.) as argument of "Apply" function of
+    "nn.Module"
+    :param m: Layer to initialize
+    :return: None
+    """
+    if isinstance(m, nn.Conv2d):
+        '''
+        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(m.weight)
+        trunc_normal_(m.weight, std=math.sqrt(1.0/fan_in)/.87962566103423978)
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+        '''
+        nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
+        if m.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(m.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(m.bias, -bound, bound)
+        
+    elif isinstance(m, nn.BatchNorm2d):
+        nn.init.constant_(m.weight, 1)
+        nn.init.constant_(m.bias, 0)
+
+class Conv(nn.Module):
+    def __init__(self, inp_dim, out_dim, kernel_size=3, stride=1, bn=False, relu=True, bias=True):
+        super(Conv, self).__init__()
+        self.inp_dim = inp_dim
+        self.conv = nn.Conv2d(inp_dim, out_dim, kernel_size, stride, padding=(kernel_size-1)//2, bias=bias)
+        self.relu = None
+        self.bn = None
+        if relu:
+            self.relu = nn.ReLU(inplace=True)
+        if bn:
+            self.bn = nn.BatchNorm2d(out_dim)
+
+    def forward(self, x):
+        assert x.size()[1] == self.inp_dim, "{} {}".format(x.size()[1], self.inp_dim)
+        # print("++",x.size()[1],self.inp_dim,x.size()[1],self.inp_dim)
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size=1, stride=1, padding=0, dilation=1):
+        super(BasicConv2d, self).__init__()
+
+        self.conv = nn.Conv2d(in_planes, out_planes,
+                              kernel_size=kernel_size, stride=stride,
+                              padding=padding, dilation=dilation, bias=False)
+        self.bn = nn.BatchNorm2d(out_planes)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        return x
+
+
+class CPAMEnc(nn.Module):
+    def __init__(self, in_channels, norm_layer):
+        super(CPAMEnc, self).__init__()
+        self.pool1 = nn.AdaptiveAvgPool2d(1)
+        self.pool2 = nn.AdaptiveAvgPool2d(2)
+        self.pool3 = nn.AdaptiveAvgPool2d(3)
+        self.pool4 = nn.AdaptiveAvgPool2d(6)
+
+        self.conv1 = nn.Sequential(nn.Conv2d(in_channels, in_channels, 1, bias=False),
+                                norm_layer(in_channels),
+                                nn.ReLU(True))
+        self.conv2 = nn.Sequential(nn.Conv2d(in_channels, in_channels, 1, bias=False),
+                                norm_layer(in_channels),
+                                nn.ReLU(True))
+        self.conv3 = nn.Sequential(nn.Conv2d(in_channels, in_channels, 1, bias=False),
+                                norm_layer(in_channels),
+                                nn.ReLU(True))
+        self.conv4 = nn.Sequential(nn.Conv2d(in_channels, in_channels, 1, bias=False),
+                                norm_layer(in_channels),
+                                nn.ReLU(True))
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+        
+        feat1 = self.conv1(self.pool1(x)).view(b,c,-1)
+        feat2 = self.conv2(self.pool2(x)).view(b,c,-1)
+        feat3 = self.conv3(self.pool3(x)).view(b,c,-1)
+        feat4 = self.conv4(self.pool4(x)).view(b,c,-1)
+        
+        return torch.cat((feat1, feat2, feat3, feat4), 2)
+
+class CPAMDec(nn.Module):
+    def __init__(self,in_channels):
+        super(CPAMDec,self).__init__()
+        self.softmax  = nn.Softmax(dim=-1)
+        self.scale = nn.Parameter(torch.zeros(1))
+
+        self.conv_query = nn.Conv2d(in_channels = in_channels , out_channels = in_channels//4, kernel_size= 1) 
+        self.conv_key = nn.Linear(in_channels, in_channels//4) 
+        self.conv_value = nn.Linear(in_channels, in_channels) 
+    def forward(self, x,y):
+        m_batchsize,C,width ,height = x.size()
+        m_batchsize,K,M = y.size()
+
+        proj_query  = self.conv_query(x).view(m_batchsize,-1,width*height).permute(0,2,1)
+        proj_key =  self.conv_key(y).view(m_batchsize,K,-1).permute(0,2,1)
+        energy =  torch.bmm(proj_query,proj_key)
+        attention = self.softmax(energy)
+
+        proj_value = self.conv_value(y).permute(0,2,1) 
+        out = torch.bmm(proj_value,attention.permute(0,2,1))
+        out = out.view(m_batchsize,C,width,height)
+        out = self.scale*out + x
+        return out
+
+class CCAMDec(nn.Module):
+    def __init__(self):
+        super(CCAMDec,self).__init__()
+        self.softmax  = nn.Softmax(dim=-1)
+        self.scale = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x,y):
+        m_batchsize,C,width ,height = x.size()
+        x_reshape =x.view(m_batchsize,C,-1)
+
+        B,K,W,H = y.size()
+        y_reshape =y.view(B,K,-1)
+        proj_query  = x_reshape 
+        proj_key  = y_reshape.permute(0,2,1) 
+        energy =  torch.bmm(proj_query,proj_key)
+        energy_new = torch.max(energy,-1,keepdim=True)[0].expand_as(energy)-energy
+        attention = self.softmax(energy_new)
+        proj_value = y.view(B,K,-1) 
+        
+        out = torch.bmm(attention,proj_value) 
+        out = out.view(m_batchsize,C,width ,height)
+
+        out = x + self.scale*out
+        return out
+
+class CPAMDec_Mix(nn.Module):
+    """
+    CPAM decoding module
+    """
+    def __init__(self,in_channels):
+        super(CPAMDec_Mix,self).__init__()
+        self.softmax  = nn.Softmax(dim=-1)
+        self.scale = nn.Parameter(torch.zeros(1))
+
+        self.conv_query1 = nn.Conv2d(in_channels = in_channels , out_channels = in_channels//4, kernel_size= 1) # query_conv2
+        self.conv_key1 = nn.Linear(in_channels, in_channels//4) # key_conv2
+        self.conv_value1 = nn.Linear(in_channels, in_channels) # value2
+
+        self.conv_query2 = nn.Conv2d(in_channels = in_channels , out_channels = in_channels//4, kernel_size= 1) # query_conv2
+        self.conv_key2 = nn.Linear(in_channels, in_channels//4) # key_conv2
+        self.conv_value2 = nn.Linear(in_channels, in_channels) # value2
+
+
+    def forward(self,x1,y1,x2,y2):
+        m_batchsize,C,width ,height = x1.size()
+        m_batchsize,K,M = y1.size()
+
+        proj_query1  = self.conv_query1(x1).view(m_batchsize,-1,width*height).permute(0,2,1)
+        proj_key1 =  self.conv_key1(y1).view(m_batchsize,K,-1).permute(0,2,1)
+        proj_value1 = self.conv_value1(y1).permute(0,2,1)
+
+        proj_query2  = self.conv_query2(x2).view(m_batchsize,-1,width*height).permute(0,2,1)
+        proj_key2 =  self.conv_key2(y2).view(m_batchsize,K,-1).permute(0,2,1)
+        proj_value2 = self.conv_value2(y2).permute(0,2,1) 
+
+        energy1 =  torch.bmm(proj_query1,proj_key1)
+        energy2 =  torch.bmm(proj_query2,proj_key2)
+
+        energy = torch.abs(energy1-energy2)
+        attention = self.softmax(energy) 
+
+        out1 = torch.bmm(proj_value1,attention.permute(0,2,1))
+        out1 = out1.view(m_batchsize,C,width,height)
+        out1 = self.scale*out1 + x1 
+
+        out2 = torch.bmm(proj_value2,attention.permute(0,2,1))
+        out2 = out2.view(m_batchsize,C,width,height)
+        out2 = self.scale*out2 + x2 
+        return out1, out2
+
+class ContrastiveAtt(nn.Module):
+    def __init__(self, in_channels, out_channels, norm_layer=nn.BatchNorm2d):
+        super(ContrastiveAtt, self).__init__()
+
+        inter_channels = in_channels // 2
+
+        self.conv_cpam_b_x = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+                                   norm_layer(inter_channels),
+                                   nn.ReLU()) # conv5_s
+        self.conv_cpam_b_y = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+                                   norm_layer(inter_channels),
+                                   nn.ReLU()) # conv5_s
+
+        self.cpam_enc_x = CPAMEnc(inter_channels, norm_layer) # en_s
+        self.cpam_enc_y = CPAMEnc(inter_channels, norm_layer) # en_s
+
+        self.cpam_dec_mix = CPAMDec_Mix(inter_channels) # de_s
+
+        self.conv_cat = nn.Sequential(nn.Conv2d(inter_channels*2, out_channels, 3, padding=1, bias=False),
+                                   norm_layer(out_channels),
+                                   nn.ReLU()) 
+        
+    def forward(self, x, y):
+        cpam_b_x = self.conv_cpam_b_x(x)
+        cpam_f_x = self.cpam_enc_x(cpam_b_x).permute(0,2,1)
+
+        cpam_b_y = self.conv_cpam_b_y(y)
+        cpam_f_y = self.cpam_enc_y(cpam_b_y).permute(0,2,1)
+
+        cpam_feat1, cpam_feat2 = self.cpam_dec_mix(cpam_b_x,cpam_f_x,cpam_b_y,cpam_f_y) 
+
+        feat_sum = self.conv_cat(torch.cat([cpam_feat1,cpam_feat2],1))
+        return feat_sum, cpam_feat1, cpam_feat2
+
+class DRAtt(nn.Module):
+    def __init__(self, in_channels, out_channels, norm_layer=nn.BatchNorm2d):
+        super(DRAtt, self).__init__()
+
+        inter_channels = in_channels // 2
+
+        self.conv_cpam_b = nn.Sequential(nn.Conv2d(inter_channels, out_channels, 3, padding=1, bias=False),
+                                   norm_layer(out_channels),
+                                   nn.ReLU()) # conv5_s
+        self.cpam_enc = CPAMEnc(out_channels, norm_layer) # en_s
+        self.cpam_dec = CPAMDec(out_channels) # de_s
+
+        self.conv_ccam_b = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+                                   norm_layer(inter_channels),
+                                   nn.ReLU()) 
+        self.ccam_enc = nn.Sequential(nn.Conv2d(inter_channels, inter_channels//16, 1, bias=False),
+                                   norm_layer(inter_channels//16),
+                                   nn.ReLU()) 
+        self.ccam_dec = CCAMDec()
+        
+    def forward(self, x):
+        ccam_b = self.conv_ccam_b(x)
+        ccam_f = self.ccam_enc(ccam_b)
+        ccam_feat = self.ccam_dec(ccam_b,ccam_f)        
+        
+        cpam_b = self.conv_cpam_b(ccam_feat)
+        cpam_f = self.cpam_enc(cpam_b).permute(0,2,1)#BKD
+        cpam_feat = self.cpam_dec(cpam_b,cpam_f)
+        return cpam_feat
+
+class CICNet(nn.Module):
+    def __init__(self, num_classes=2, drop_rate=0.2, normal_init=True, pretrained=False):
+        super(CICNet, self).__init__()
+        
+        self.resnet = resnet18()
+        self.resnet.load_state_dict(torch.load('./pretrained/resnet18-5c106cde.pth'))
+        # self.resnet = resnet34()
+        # self.resnet.load_state_dict(torch.load('./pretrained/resnet34-b627a593.pth'))
+        self.resnet.layer4 = nn.Identity()
+
+        self.consrative2 = ContrastiveAtt(256,128)
+        self.consrative3 = ContrastiveAtt(128,64)
+        self.consrative4 = ContrastiveAtt(64,32)
+
+        self.Translayer2_1 = BasicConv2d(128,64,1)
+        self.fam32_1 = DRAtt(128,64) 
+        self.Translayer3_1 = BasicConv2d(64,32,1)
+        self.fam43_1 = DRAtt(64,32)
+
+        self.Translayer2_2 = BasicConv2d(128,64,1)
+        self.fam32_2 = DRAtt(128,64) 
+        self.Translayer3_2 = BasicConv2d(64,32,1)
+        self.fam43_2 = DRAtt(64,32) 
+
+        self.upsamplex2 = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.upsamplex4 = nn.Upsample(scale_factor=4, mode='bilinear')
+        self.upsamplex8 = nn.Upsample(scale_factor=8, mode='bilinear')
+        
+        self.final1 = nn.Sequential(
+            Conv(32, 32, 3, bn=True, relu=True),
+            Conv(32, num_classes, 3, bn=False, relu=False)
+            )
+        self.final2 = nn.Sequential(
+            Conv(32, 32, 3, bn=True, relu=True),
+            Conv(32, num_classes, 3, bn=False, relu=False)
+            )
+        self.final_middle_1 = nn.Sequential(
+            Conv(64, 32, 3, bn=True, relu=True),
+            Conv(32, num_classes, 3, bn=False, relu=False)
+            )
+        self.final_middle_2 = nn.Sequential(
+            Conv(64, 32, 3, bn=True, relu=True),
+            Conv(32, num_classes, 3, bn=False, relu=False)
+            )
+
+        if normal_init:
+            self.init_weights()
+
+    def forward(self, imgs1, imgs2, labels=None):
+        c0 = self.resnet.conv1(imgs1)
+        c0 = self.resnet.bn1(c0)
+        c0 = self.resnet.relu(c0)
+        c1 = self.resnet.maxpool(c0)
+        c0_img2 = self.resnet.conv1(imgs2)
+        c0_img2 = self.resnet.bn1(c0_img2)
+        c0_img2 = self.resnet.relu(c0_img2)
+        c1_img2 = self.resnet.maxpool(c0_img2)
+
+        c1 = self.resnet.layer1(c1)
+        c1_img2 = self.resnet.layer1(c1_img2)
+        
+        c2 = self.resnet.layer2(c1) 
+        c2_img2 = self.resnet.layer2(c1_img2) 
+
+        c3 = self.resnet.layer3(c2) 
+        c3_img2 = self.resnet.layer3(c2_img2) 
+
+        cross_result4, cur1_4, cur2_4 = self.consrative4(c1, c1_img2)  
+        cross_result3, cur1_3, cur2_3 = self.consrative3(c2, c2_img2) 
+        cross_result2, cur1_2, cur2_2 = self.consrative2(c3, c3_img2) 
+
+        out2 = self.Translayer2_1(cross_result2)
+        out3 = self.fam32_1(torch.cat([cross_result3, self.upsamplex2(out2)],1))
+        out4 = self.fam43_1(torch.cat([cross_result4, self.upsamplex2(self.Translayer3_1(out3))],1))
+
+        out2_2 = self.Translayer2_2(torch.abs(cur1_2-cur2_2))
+        out3_2 = self.fam32_2(torch.cat([torch.abs(cur1_3-cur2_3), self.upsamplex2(out2_2)],1))
+        out4_2 = self.fam43_2(torch.cat([torch.abs(cur1_4-cur2_4), self.upsamplex2(self.Translayer3_2(out3_2))],1)) 
+
+        out_1 = self.final1(self.upsamplex4(out4))
+        out_2 = self.final2(self.upsamplex4(out4_2))
+        out_middle_1 = self.final_middle_1(self.upsamplex8(out3))
+        out_middle_2 = self.final_middle_2(self.upsamplex8(out3_2))
+
+        return out_1, out_2, out_middle_1, out_middle_2 
+
+    def init_weights(self):
+        self.consrative2.apply(init_weights)
+        self.consrative3.apply(init_weights)        
+        self.consrative4.apply(init_weights)
+
+        self.fam32_1.apply(init_weights)
+        self.Translayer2_1.apply(init_weights)
+        self.fam43_1.apply(init_weights)
+        self.Translayer3_1.apply(init_weights)
+
+        self.fam32_2.apply(init_weights)
+        self.Translayer2_2.apply(init_weights)
+        self.fam43_2.apply(init_weights)
+        self.Translayer3_2.apply(init_weights)
+
+        self.final1.apply(init_weights)
+        self.final2.apply(init_weights)
+        self.final_middle_1.apply(init_weights)
+        self.final_middle_2.apply(init_weights)
+
+def init_conv(conv, glu=True):
+    nn.init.xavier_uniform_(conv.weight)
+    if conv.bias is not None:
+        conv.bias.data.zero_()
